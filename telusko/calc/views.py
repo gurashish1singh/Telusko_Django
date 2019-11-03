a541b23da264487a49b9187db8cfe884a4acd217
Gurashish Singh <gurashish1singh@gmail.com>
2019-11-03 13:28:36 -0500

    Finished with Calc app(added 2 templates for adding and results). Created new travello app with a functional index page(no css yet)

--- telusko/calc/views.py
+++ telusko/calc/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def home(request):
+
+    context = {
+        'name':'Gura'
+    }
+    template_name = 'calc/home.html'
+    return render(request,template_name,context)
+
+
+def add(request):
+    
+    val1 = request.POST['num1']
+    val2 = request.POST['num2']
+    res = int(val1) + int(val2)
+
+    context = {
+        'result' : res
+    }
+    template_name = 'calc/result.html'
+    return render(request,template_name,context)
--- telusko/telusko/settings.py
+++ telusko/telusko/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Installed Apps
+    'calc',
+    'travello',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'telusko.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
